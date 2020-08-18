8909cfb93067b5cac44dc3d84544847ebbfead99
belen-dominguez <belen.dominguez.sigler@outlook.com>
2020-08-18 11:46:13 -0300

    Updates

---
+++ precache-manifest.2a846dac9e1234685a6312731a597991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af40cab40a6b20ea617179259f713f04",
+    "url": "/React_TP-Movies/index.html"
+  },
+  {
+    "revision": "512b7b9f3056322442d6",
+    "url": "/React_TP-Movies/static/css/main.19bced56.chunk.css"
+  },
+  {
+    "revision": "fa6a73e84b4c838bbe7e",
+    "url": "/React_TP-Movies/static/js/2.2a4f3850.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_TP-Movies/static/js/2.2a4f3850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512b7b9f3056322442d6",
+    "url": "/React_TP-Movies/static/js/main.b3dc9fb2.chunk.js"
+  },
+  {
+    "revision": "cfb3d97159ac2e3bdee9",
+    "url": "/React_TP-Movies/static/js/runtime-main.deb46eb0.js"
+  }
+]);
